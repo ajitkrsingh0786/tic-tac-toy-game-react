eb8623b29750db9e7d9e76f81496a1b105efc6b9
ajit <ajitkrsingh0786@gmail.com>
2020-10-08 21:25:23 +0530

    2nd commit: Select option completed

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,58 +2,173 @@ import React, { Component } from 'react';
 import Board from './Board'
 class Game extends Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.state = {
+
             xIsNext: true,
             squares: Array(9).fill(null)
-           /* stemNumber:0,
-            history: [
-                { squares: Array(9).fill(null) }
-            ]*/
+
         }
     }
-     
-     handleClick(i){
-         //console.log(i);
-        // const history = this.state.history.slice(0,this.state.stemNumber+1);
-        // const current = history[history.length-1];
-        // const squares = current.squares.slice();
-         //squares[i] = this.state.xIsNext?'X':'0';
-         //   console.log(squares);
-         const squares = this.state.squares.slice();
-         if(squares[i]===null){
-          
-         squares[i] = this.state.xIsNext?'X':'0';
 
-         this.setState({
+    handleClick(i) {
 
-               squares: squares,
-            // history: history.concat({
-           //      squares: squares 
-           //  }),
-             xIsNext: !this.state.xIsNext,
-             //stepNumber: history.length,
-             
-         })}else{
-         alert("this box is already filled");}
-       // console.log(this.state.history);
-          
-     }
-       
+        const squares = this.state.squares.slice();
+        if (squares[i] === null) {
+            squares[i] = this.state.xIsNext ? 'X' : '0';
+            let time = 150;
+            if (squares[0] === squares[1] && squares[0] === squares[2] && squares[0] !== null) {
+
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+
+            } else if (squares[3] === squares[4] && squares[3] === squares[5] && squares[3] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+
+            } else if (squares[6] === squares[7] && squares[6] === squares[8] && squares[6] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+
+            } else if (squares[0] === squares[3] && squares[0] === squares[6] && squares[0] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+            } else if (squares[1] === squares[4] && squares[1] === squares[7] && squares[1] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+
+            } else if (squares[2] === squares[5] && squares[2] === squares[8] && squares[2] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+            } else if (squares[0] === squares[4] && squares[0] === squares[8] && squares[0] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+            } else if (squares[i] === squares[4] && squares[2] === squares[6] && squares[2] !== null) {
+                this.setState({
+                    squares: squares,
+                    xIsNext: !this.state.xIsNext,
+                })
+
+                setTimeout(() => {
+                    alert(squares[i] === 'X' ? "First player has won" : "Second player has won");
+                    this.setState({
+                        squares: Array(9).fill(null),
+                        xIsNext: true,
+                    });
+                }, time);
+            } else {
+
+                if (squares[0] !== null && squares[1] !== null && squares[2] !== null && squares[3] !== null && squares[4] !== null &&
+                    squares[5] !== null && squares[6] !== null && squares[7] !== null && squares[8] !== null) {
+
+                        this.setState({
+                            squares: squares,
+                            xIsNext: !this.state.xIsNext,
+                        })
+
+                    setTimeout(() => {
+                        alert("Match Draw");
+                        this.setState({
+                            squares: Array(9).fill(null),
+                            xIsNext: true,
+                        });
+                    }, time);
+                } else {
+                    this.setState({
+                        squares: squares,
+                        xIsNext: !this.state.xIsNext,
+                    })
+                }
+            }
+
+        } else {
+            alert("this box is already filled");
+        }
+
+
+    }
+
     render() {
-               //const history = this.state.history;
-               //console.log( history);
-               //console.log(this.state.stemNumber);
-              // const current = history[this.state.stemNumber];
-              // console.log(current);
-               //console.log(current.squares);
-        return(
+
+        return (
             <div className="game">
                 <div className="game-board">
-                    <Board onClick={ (i) => this.handleClick(i)}
-                    squares={this.state.squares}/>
+                    <Board onClick={(i) => this.handleClick(i)}
+                        squares={this.state.squares} />
                 </div>
-                
+
             </div>
         )
     }
